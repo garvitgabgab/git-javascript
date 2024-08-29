d87a433958584ef93c9b4a85869d77228ab96f93
Garvit <gaurgarvit64@gmail.com>
2024-08-29 15:23:00 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -11,33 +11,27 @@ switch (command) {
     break;
 
   case "cat-file":
-    const flag = process.argv[3];
-    const blobSHA = process.argv[4];
-    if (flag === "-p") {
-      printBlob(blobSHA);
+    const catFlag = process.argv[3];
+    const catHash = process.argv[4];
+    if (catFlag === "-p") {
+      printBlob(catHash);
     } else {
-      throw new Error(`Unknown flag ${flag}`);
+      throw new Error(`Unknown flag ${catFlag}`);
     }
     break;
 
   case "hash-object":
-    const writeFlag = process.argv[3];
+    const hashFlag = process.argv[3];
     const fileName = process.argv[4];
-    if (writeFlag === "-w") {
-      saveBlob(fileName);
+    if (hashFlag === "-w") {
+      hashObject(fileName);
     } else {
-      throw new Error(`Unknown flag ${writeFlag}`);
+      throw new Error(`Unknown flag ${hashFlag}`);
     }
     break;
 
   case "ls-tree":
-    const lsFlag = process.argv[3];
-    const treeSHA = process.argv[4];
-    if (lsFlag === "--name-only") {
-      listTree(treeSHA);
-    } else {
-      throw new Error(`Unknown flag ${lsFlag}`);
-    }
+    listTree(process.argv[3]);
     break;
 
   case "write-tree":
@@ -49,84 +43,78 @@ switch (command) {
 }
 
 function createGitDirectory() {
-  mkdirSync(join(__dirname, ".git"), { recursive: true });
-  mkdirSync(join(__dirname, ".git", "objects"), { recursive: true });
-  mkdirSync(join(__dirname, ".git", "refs"), { recursive: true });
-
-  writeFileSync(join(__dirname, ".git", "HEAD"), "ref: refs/heads/main\n");
+  mkdirSync(join(process.cwd(), ".git"), { recursive: true });
+  mkdirSync(join(process.cwd(), ".git", "objects"), { recursive: true });
+  mkdirSync(join(process.cwd(), ".git", "refs"), { recursive: true });
+  writeFileSync(join(process.cwd(), ".git", "HEAD"), "ref: refs/heads/main\n");
   console.log("Initialized git directory");
 }
 
 function readObject(hash) {
-  const rawData = readFileSync(join(__dirname, ".git", "objects", hash.slice(0, 2), hash.slice(2)));
+  const filePath = join(process.cwd(), ".git", "objects", hash.slice(0, 2), hash.slice(2));
+  const rawData = readFileSync(filePath);
   const data = inflateSync(rawData);
   return data.toString();
 }
 
 function writeObject(hash, content) {
-  const dir = join(__dirname, ".git", "objects", hash.slice(0, 2));
+  const dir = join(process.cwd(), ".git", "objects", hash.slice(0, 2));
   mkdirSync(dir, { recursive: true });
   writeFileSync(join(dir, hash.slice(2)), deflateSync(content));
 }
 
-function printBlob(blobSHA) {
-  const content = readObject(blobSHA).split("\x00")[1];
+function hashObject(filePath) {
+  const fileData = readFileSync(filePath);
+  const header = `blob ${fileData.length}\0`;
+  const content = Buffer.concat([Buffer.from(header), fileData]);
+  const hash = createHash("sha1").update(content).digest("hex");
+  writeObject(hash, content);
+  console.log(hash);
+}
+
+function printBlob(hash) {
+  if (!hash) throw new Error("Missing hash");
+  const content = readObject(hash).split("\0")[1];
   process.stdout.write(content);
 }
 
-function saveBlob(filePath) {
-  if (!filePath) {
-    throw new Error("Missing filename");
-  }
-  const fileData = readFileSync(filePath);
-  const data = `blob ${statSync(filePath).size}\x00${fileData}`;
-  const hash = createHash("sha1").update(data).digest("hex");
-  writeObject(hash, data);
-  process.stdout.write(hash);
-}
-
-function listTree(treeSHA) {
-  if (!treeSHA) {
-    throw new Error("Missing hash");
-  }
-  const content = readObject(treeSHA).split("\x00").slice(1).join("\x00");
-  const entries = [...content.matchAll(/(\d+) (.*?)\0(.{20})/g)];
-  console.log(entries.map(([, , name]) => name).join("\n"));
+function listTree(treeHash) {
+  if (!treeHash) throw new Error("Missing tree hash");
+  const treeData = readObject(treeHash);
+  const entries = [...treeData.matchAll(/(\d+) (.*?)\0(.{20})/g)];
+  entries.forEach(([, , name]) => console.log(name));
 }
 
 function writeTree() {
-  const hash = writeTreeForPath(".");
-  process.stdout.write(hash);
+  const rootHash = writeTreeForPath(".");
+  process.stdout.write(rootHash);
 }
 
-function writeTreeForPath(directory) {
-  const dirContent = readdirSync(directory);
-  const entries = dirContent
-    .filter((name) => name !== ".git" && name !== "main.js")
-    .map((name) => {
-      const fullPath = join(directory, name);
-      const stat = statSync(fullPath);
-      if (stat.isDirectory()) {
-        return ["040000", name, writeTreeForPath(fullPath)];
-      } else if (stat.isFile()) {
-        return ["100644", name, saveFileAsBlob(fullPath)];
-      }
-      return ["", "", ""];
-    })
-    .sort((a, b) => a[1].localeCompare(b[1]))
-    .reduce((acc, [mode, name, hash]) => {
-      return Buffer.concat([acc, Buffer.from(`${mode} ${name}\x00`), Buffer.from(hash, "hex")]);
-    }, Buffer.alloc(0));
-  
-  const tree = Buffer.concat([Buffer.from(`tree ${entries.length}\x00`), entries]);
+function writeTreeForPath(dirPath) {
+  const entries = readdirSync(dirPath).filter(name => name !== ".git").map(name => {
+    const fullPath = join(dirPath, name);
+    const stats = statSync(fullPath);
+
+    if (stats.isDirectory()) {
+      const treeHash = writeTreeForPath(fullPath);
+      return [`040000`, name, treeHash];
+    } else if (stats.isFile()) {
+      const blobHash = hashObject(fullPath);
+      const mode = (stats.mode & 0o111) ? "100755" : "100644";
+      return [mode, name, blobHash];
+    }
+
+    return [];
+  });
+
+  const entryBuffers = entries.map(([mode, name, hash]) => 
+    Buffer.concat([Buffer.from(`${mode} ${name}\0`), Buffer.from(hash, "hex")])
+  );
+  const treeData = Buffer.concat(entryBuffers);
+  const header = Buffer.from(`tree ${treeData.length}\0`);
+  const tree = Buffer.concat([header, treeData]);
   const hash = createHash("sha1").update(tree).digest("hex");
-  writeObject(hash, tree);
-  return hash;
-}
 
-function saveFileAsBlob(file) {
-  const data = `blob ${statSync(file).size}\x00${readFileSync(file)}`;
-  const hash = createHash("sha1").update(data).digest("hex");
-  writeObject(hash, data);
+  writeObject(hash, tree);
   return hash;
 }
